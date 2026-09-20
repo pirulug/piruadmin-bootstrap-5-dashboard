818e101644b919f0c3eb94ed95445c2c99305956
pirulug <pirulug@gmail.com>
2026-09-20 15:38:05 -0500

    feat: add top navigation component and dark/light mode module

--- src/js/modules/darkLightMode.js
+++ src/js/modules/darkLightMode.js
@@ -38,8 +38,8 @@
       <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
     </svg>`,
     check: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
-      <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-    </svg>`
+  <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+</svg>`
   };
 
   const updateThemeIcon = (theme) => {
